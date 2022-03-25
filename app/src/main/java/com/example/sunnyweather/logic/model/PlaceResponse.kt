f8d1fc13f546393533d1b20afec20dc1a4553529
ganlingzhi <s-ganlingzhi@nowcoder.com>
2022-03-25 16:50:13 +0800

    实现全球搜索功能

---
+++ app/src/main/java/com/example/sunnyweather/logic/model/PlaceResponse.kt
@@ -0,0 +1,12 @@
+package com.example.sunnyweather.logic.model
+
+import com.google.gson.annotations.SerializedName
+
+data class PlaceResponse(val status: String, val query: String, val places: List<Place>)
+data class Place(
+    val name: String,
+    val location: Location,
+    @SerializedName("formatted_address") val address: String
+)
+
+data class Location(val lat: String, val lng: String)
